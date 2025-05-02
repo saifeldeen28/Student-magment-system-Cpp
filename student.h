5e5df9f89d66e9ae0ed00b6ea38a8d3effe4ff23
saif eldeen <saifeldeen.anada@gmail.com>
2025-05-03 00:27:37 +0300

    extra commit

---
+++ student.h
@@ -0,0 +1,95 @@
+
+#ifndef STUDENT_H
+#define STUDENT_H
+#include <iostream>
+#include <vector>
+#include "Course.h"
+#include "User.h"
+using namespace std ;
+class Student : public User {
+public:
+    vector<Course*> Registered_Courses ;
+    vector<int> Grades ;
+    int MAX_COURSES = 6 ;
+    Student();
+    Student(string username, string password, int id);
+    bool set_Courses_Grades(vector<Course*> courses , vector<int> grades ) {
+        if (courses.size() > MAX_COURSES) {
+            cout<< "You have reached the maximum number of courses" << endl ;
+            return false ;
+        }
+        if (courses.size()!= grades.size()) {
+            cout<< "Number of courses and grades must match" << endl ;
+            return false;
+        }
+
+        Registered_Courses = courses ;
+        Grades = grades ;
+
+        cout<< "Courses and grades have been assigned successfully" << endl ;
+        return true ;
+    }
+    bool add(Course* course) {
+        if (Registered_Courses.size() >= MAX_COURSES) {
+            cout<< "You have reached the maximum number of courses" << endl ;
+            return false;
+        }
+
+        for (int i = 0; i < Registered_Courses.size(); i++) {
+            if (Registered_Courses[i]->code == course->code) {
+                cout<< "Already registered for course ID: " << course->code<< endl ;
+                return false;
+            }
+        }
+
+        Registered_Courses.push_back(course);
+        Grades.push_back(0);
+
+        cout<< "Successfully registered for course ID: " << course->code << endl ;
+        return true ;
+    }
+    bool drop(int courseID) {
+        for (int i = 0 ; i < Registered_Courses.size() ; i++) {
+            if (Registered_Courses[i]->code == courseID) {
+                Registered_Courses.erase(Registered_Courses.begin() + i);
+                if (i < Grades.size()) {
+                    Grades.erase(Grades.begin() + i);
+                }
+                cout<< "Successfully dropped course ID:"<< courseID << endl  ;
+                return true;
+            }
+        }
+
+        cout<< "Course ID " << courseID << " not found in your registered courses   " << endl ;
+        return false;
+    }
+    void viewGrades() {
+        if (Registered_Courses.empty()) {
+            cout<< "You are not registered in any courses" << endl ;
+            return;
+        }
+
+        cout << "Your Grades:" << endl ;
+        for (int i = 0; i < Registered_Courses.size(); i++) {
+            cout<< "Course: " << Registered_Courses[i]->name << " ID: " << Registered_Courses[i]->code<< "Grade: " << Grades[i] << endl ;
+        }
+    }
+    void averageGrade() {
+        if (Grades.empty()) {
+            cout<< "No grades available to calculate average." << endl ;
+            return ;
+        }
+
+        int sum = 0;
+        for (int i = 0 ; i < Grades.size() ; i++) {
+            sum += Grades[i];
+        }
+
+        double average = (double)sum / Grades.size() ;
+        cout << "Average grade: " << average << endl ;
+    }
+
+};
+
+#endif // STUDENT_H
+
