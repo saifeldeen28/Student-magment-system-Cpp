766389734d450c8b0092f43082b67ace69369173
saif eldeen <saifeldeen.anada@gmail.com>
2025-05-04 23:53:11 +0300

    extra commit

--- Course.h
+++ Course.h
@@ -19,26 +19,10 @@ public:
     vector<int> student_ids;
 
     // Improved constructor with initializer list
-    Course(string name,const int code, int credits, const vector<int> grades)
-        : name(move(name)), code(code), credits(credits), grades(grades) {}
+    Course(string name,const int code, int credits, const vector<int> &grades, const vector<int> &student_ids, const vector<int> &instructors_ids)
+        : name(move(name)), code(code), credits(credits), grades(grades),student_ids(student_ids), instructors_ids(instructors_ids) {}
 
-    // Improved instructor ID setter function
-    bool set_instructors_ids(const vector<int>& ids, const vector<int>& all_ids) {
-        for (int id : ids) {
-            bool exists = false;
-            for (int valid_id : all_ids) {
-                if (id == valid_id) {
-                    exists = true;
-                    break; // Found the ID, no need to keep checking
-                }
-            }
-            if (!exists) {
-                return false; // If any ID doesn't exist, return false immediately
-            }
-        }
-        instructors_ids = ids;
-        return true;
-    }
+
     void print_grades() {
         for (int i = 0; i < grades.size(); i++) {
             cout<<grades.at(i)<<endl;
@@ -50,9 +34,18 @@ public:
         }
     }
     int getGrade(int id){
+        int flag=0;
+        for (int i = 0; i < student_ids.size(); i++) {
+            if (student_ids.at(i) == id) {
+                flag=1;
+            }
+        }
+        if (flag==0) {
+            return -1;//no student found
+        }
         int index;
-        for(int i=0 ; i,stu_ids.size();i++){
-            if(id==stu_ids[i]){
+        for(int i=0 ; i<student_ids.size();i++){
+            if(id==student_ids[i]){
                 index=i;
                 break;
             }
--- main.cpp
+++ main.cpp
@@ -2,17 +2,17 @@
 #include <vector>
 #include "User.h"
 #include "Course.h"
+#include "student.h"
 using namespace std;
 int main() {
     //authentication
     User u1(1,"s","2");
-    vector<int> id {1,2};
-    vector<int> all{1,2,35,6,7};
-    vector<int> al{1,2,35,6,7};
+    vector<int> grades{1,2,35,6,7};
+    vector<int> ids {1,2,35,6,7};
+    vector<int> instructors_ids {1,2,35,6,7};
 
-    Course c1("n",1,3,al);
-    cout<<c1.set_instructors_ids(id,all);
-    c1.print_instructors_ids();
+    Course c1("n",1,3,grades,ids,instructors_ids);
+    Student s1(1,"s","2");
 
 
 
