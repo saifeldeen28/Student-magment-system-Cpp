be9a7e0f974a4f3177426afbba2f2093c94cde24
Karimwael7 <karimwael06@gmail.com>
2025-05-04 12:00:10 +0300

    Update Course.h

--- Course.h
+++ Course.h
@@ -16,6 +16,7 @@ public:
     vector<int> instructors_ids;
     int credits;
     vector<int> grades;
+    vector<int> student_ids;
 
     // Improved constructor with initializer list
     Course(string name,const int code, int credits, const vector<int> grades)
